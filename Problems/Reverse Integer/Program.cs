3d73d0613831825e3bad4fc02fc9d286038e4592
RayyFarr <dohvakiin555@yahoo.com>
2024-01-02 18:06:47 +0600

    Solved Reverse Integer
    Added A solution to the Reverse Integer Problem

---
+++ Problems/Reverse Integer/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Numerics;
+
+Console.WriteLine(Reverse(1534236469));
+Console.WriteLine(Reverse(-123));
+Console.WriteLine(Reverse(120));
+
+
+//Solution 1.
+/*int Reverse(int x){
+   BigInteger n = Math.Sign(x) * Convert.ToInt64(new string(x.ToString().ToCharArray().Reverse().ToArray()).Replace("-", ""));
+    if (n < int.MaxValue && n > int.MinValue)
+        return (int)n;
+    return 0;
+}
+*/
+
+//Solution 2 faster.
+int Reverse(int x)
+{
+    try
+    {
+        return Math.Sign(x) *
+            Convert.ToInt32(new string(x.ToString().ToCharArray().Reverse().ToArray()).Replace("-", ""));
+    }
+    catch
+    {
+        return 0;
+    }
+}
