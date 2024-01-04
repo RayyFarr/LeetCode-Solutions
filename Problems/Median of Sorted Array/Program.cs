b5ff3c0c17daea2a0d2265f43586db0ea6775cc6
RayyFarr <dohvakiin555@yahoo.com>
2024-01-04 20:03:47 +0600

    Median Of Two Sorted Arrays
    Added a solution to the median of sorted arrays problem. Managed to beat about 99.11%.

---
+++ Problems/Median of Sorted Array/Program.cs
@@ -0,0 +1,9 @@
+﻿
+//Current Solution. Not too clever but still beat about 99.11%.
+double FindMedianSortedArrays(int[] nums1, int[] nums2) {
+    List<int> nums = new List<int>(nums1);
+    nums.AddRange(nums2);
+    nums.Sort();
+    if (nums.Count() % 2 != 0) return nums[nums.Count() / 2];
+    return (nums[nums.Count() / 2 - 1] + nums[nums.Count() / 2]) / 2.0;
+}
