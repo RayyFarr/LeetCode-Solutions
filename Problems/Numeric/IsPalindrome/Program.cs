1bcc6e59a18c878626f9597e12407bda49414640
RayyFarr <dohvakiin555@yahoo.com>
2024-01-09 14:08:36 +0600

    Organized Folder and added a solution
    Organized the problem into separate folders and added a solution to the palindrome number problem

---
+++ Problems/Numeric/IsPalindrome/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Console.WriteLine(IsPalindrome(121));
+Console.WriteLine(IsPalindrome(32));
+Console.WriteLine(IsPalindrome(0000));
+
+bool IsPalindrome(int x)
+{
+    
+    int tempX = x/10;
+    int rev = x%10;
+
+    while(tempX > 0)
+    {
+        rev = rev * 10 + tempX % 10;
+        tempX /= 10;       
+    }
+    return (rev == x) && Math.Sign(x) != -1;
+}
