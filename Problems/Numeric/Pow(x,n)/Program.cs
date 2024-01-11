6654591a6ba04384cc9efca482eb0b22675bd7d4
RayyFarr <dohvakiin555@yahoo.com>
2024-01-11 17:57:58 +0600

    Leaf-Similiar Trees
    Added a solution to the leaf similar tree problem and added a refined solution to the power problem by using Binary Exponentiation.

--- Problems/Numeric/Pow(x,n)/Program.cs
+++ Problems/Numeric/Pow(x,n)/Program.cs
@@ -1,15 +1,23 @@
-﻿
-Console.WriteLine(MyPow(3, 3));
+﻿Console.WriteLine(MyPow(2, 10));
+Console.WriteLine(MyPow(-2, 21231232));
+Console.WriteLine(MyPow(3, -22));
 
+//Uses Binary Exponentiation.
 double MyPow(double x, int n)
 {
-	double y = x;
-	if (n > 0)
-		while (n-- > 1)
-			x *= y;
-	else
-		while (n++ < 1)
-			x *= 1 / y;
-	return x;
+    if (n == 0 || x == 1) return 1;
+    bool inverse = n < 0;
+    double mult = 1;
+    while (n * Math.Sign(n)  != 1)
+    {
+        if (n % 2 != 0)
+        {
+            mult *= x;
+        }
+        x *= x;
+
+        n /= 2;
+    }
+    return inverse ? 1 / (mult * x) : (mult * x);
 }
-//2nd method
+//for 2^7 it looks like 2^7 => 2^6 * 2 => 4^3 * 2 => 4^2 * 2 * 4 => 16^1 * 2 * 4 => 32 * 4 => 128
---
+++ Problems/Trees/Leaf-Similiar Trees/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+//Uses the depth first search algorithm to get the leafs of the trees and checks if the lists are equal. 
+
+//Managed to beat 92% on runtime. Will make improvements later to reduce space complexity.
+bool LeafSimilar(TreeNode root1, TreeNode root2)
+{
+    //Initialize the leaf lists.
+    List<int> leaves1 = new List<int>();
+    List<int> leaves2 = new List<int>();
+
+    //Do DFS for both trees.
+    DFS(root1, leaves1);
+    DFS(root2, leaves2);
+
+    //Return true if both lists are equal.
+    return leaves1.SequenceEqual(leaves2);
+}
+
+//Recursive DFS method.
+void DFS(TreeNode root, List<int> leaves)
+{
+    //Return if the current node is null.
+    if (root == null) return;
+
+    //If this node has no branches then its a leaf. So add the value of the node to the leaf list.
+    if (root.right == null && root.left == null) leaves.Add(root.val);
+
+    //Do DFS for the node to the left first then for the node right.
+    DFS(root.left, leaves);
+    DFS(root.right, leaves);
+}
+
+
+
+
+//Tree Class.
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
