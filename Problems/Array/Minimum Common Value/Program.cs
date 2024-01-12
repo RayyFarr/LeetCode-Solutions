1c00f09109c57eeb4b46c251b949aa386e7558f0
RayyFarr <dohvakiin555@yahoo.com>
2024-01-12 22:30:06 +0600

    Minimum Common Value
    Added a solution to the minimum common value problem.

---
+++ Problems/Array/Minimum Common Value/Program.cs
@@ -0,0 +1,9 @@
+﻿int GetCommon(int[] nums1, int[] nums2)
+{
+    HashSet<int> hash = new HashSet<int>(nums2);
+    for (int i = 0; i < nums1.Length; i++)
+    {
+        if (hash.Contains(nums1[i])) return nums1[i];
+    }
+    return -1;
+}
