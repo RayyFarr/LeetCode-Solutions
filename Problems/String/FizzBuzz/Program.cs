98020ff03c6814e3921aff91f3aa56a2d89f01a7
RayyFarr <dohvakiin555@yahoo.com>
2024-01-13 22:52:00 +0600

    Majority Element And FizzBuzz
    Added solutions to the majority element and fizzbuzz

---
+++ Problems/String/FizzBuzz/Program.cs
@@ -0,0 +1,13 @@
+﻿//This simple solution somehow beat 99.39%.
+
+IList<string> FizzBuzz(int n)
+{
+    string[] s = new string[n];
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0) s[i - 1] = "Fizz";
+        if (i % 5 == 0) s[i - 1] += "Buzz";
+        if (i % 3 != 0 && i % 5 != 0) s[i - 1] = i.ToString();
+    }
+    return s;
+}
---
+++ Problems/String/Majority Element/Program.cs
@@ -0,0 +1,14 @@
+﻿int currentStreak = 0;
+
+int MajorityElement(int[] nums)
+{
+    Array.Sort(nums);
+    if (nums.Length == 0) return nums[0];
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] != nums[i - 1]) currentStreak = 0;
+        currentStreak++;
+        if (currentStreak > nums.Length / 2) return nums[i];
+    }
+    return nums[0];
+}
