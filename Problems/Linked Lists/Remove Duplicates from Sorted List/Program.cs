577def26aed419d92c529a146a8d96e5a704a68d
RayyFarr <dohvakiin555@yahoo.com>
2024-01-17 16:20:34 +0600

    Remove Duplicates (Linked List)
    Added a solution to the remove duplicates from linked list problem.

---
+++ Problems/Linked Lists/Remove Duplicates from Sorted List/Program.cs
@@ -0,0 +1,24 @@
+﻿ListNode DeleteDuplicates(ListNode head)
+{
+    if (head == null) return head;
+    if (head.next != null && head.val == head.next.val)
+    {
+        if (head.next.next != null) head.next = head.next.next;
+        else head.next = null;
+        DeleteDuplicates(head);
+    }
+    else if (head.next != null)
+        DeleteDuplicates(head.next);
+    return head;
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
